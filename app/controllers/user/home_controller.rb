b70431472b726508a7d9358edc336282ce25a755
Vinod Kumar <vinod.kumar@mobiloittegroup.com>
2016-09-20 11:25:03 +0530

    user section with devise gem

---
+++ app/controllers/user/home_controller.rb
@@ -0,0 +1,8 @@
+class User::HomeController < ApplicationController
+
+def dashboard
+	
+end
+
+
+end
---
+++ app/controllers/user/users_controller.rb
@@ -0,0 +1,15 @@
+class User::UsersController < ApplicationController
+
+ def new
+ 	@user=User.new
+ end
+
+ def index
+ 	
+ end
+
+ def create
+ 	
+ end
+
+end
