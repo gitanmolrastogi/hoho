53add3811e56fafa152adcc6e554e36a0fc977fa
Manish Gupta <abhishek.kushwaha@mobiloittegroup.com>
2016-10-15 10:40:41 +0530

    Push after change in Routes Controller

---
+++ app/assets/javascripts/user/routes.js
@@ -0,0 +1,30 @@
+
+//  $(document).ready(function(){
+// 	$(".link-a").click(function(){
+// 		$(".category-section").show(3000);
+// 		$(".category-main-section").hide();
+// 	});
+// });
+$(document).ready(function(){
+	$(".category-section").hide();
+});
+
+$(document).ready(function(){
+	$("#city_dropdown").change(function(){
+		var c_id=this.value;
+		// alert(c_id);
+
+ $.getScript( "/user/routes/city_details.js?city_id="+c_id, function( data) {
+ 	
+  console.log( data ); // Data returned
+  // console.log( textStatus ); // Success
+  
+  // console.log( jqxhr.status ); // 200
+  console.log( "Load was performed." );
+});
+
+	});
+    
+});
+
+	
---
+++ app/helpers/user/routes_helper.rb
@@ -0,0 +1,2 @@
+module User::RoutesHelper
+end
