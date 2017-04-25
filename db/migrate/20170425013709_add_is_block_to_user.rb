abc00bea4eebd8fdaa86d742ee9a2ae649ff6cfa
KMArti <km.arti@mobiloittegroup.com>
2017-04-25 11:41:01 -0700

    user is_block default false

--- db/migrate/20170425013709_add_is_block_to_user.rb
+++ db/migrate/20170425013709_add_is_block_to_user.rb
@@ -1,5 +1,5 @@
 class AddIsBlockToUser < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :is_block, :boolean,default: true
+    add_column :users, :is_block, :boolean,default: false
   end
 end
