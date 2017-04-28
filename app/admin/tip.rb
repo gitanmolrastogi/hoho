49b23f8f171b5e390db8f8edbffe5566274004bf
KMArti <km.arti@mobiloittegroup.com>
2017-04-27 18:13:35 -0700

    resolved issue

--- app/admin/tip.rb
+++ app/admin/tip.rb
@@ -9,8 +9,8 @@ filter :content
     id_column
     column :title
     column "Content" do |body|
-              truncate(body.content, omision: "...", length: 100)
-     end
+     truncate(body.try(:content).html_safe, omision: "...", length: 100, :escape => false)
+    end
     column :created_at
     actions name: "Actions"
   end
@@ -18,10 +18,22 @@ filter :content
 show :title=> "Admin User Info" do |ad|
     attributes_table do  
     row  :title
-    row  :content
+    row  :content do |resource|
+      resource.try(:content).html_safe
+    end
     row  :created_at
     row  :updated_at
    end
 end
+
+form do |f|
+  f.inputs "#{f.title}" do     
+    f.input :title, :label => "Title" 
+    f.input :content, as:  :ckeditor
+  end
+  f.actions
+end
+
+
 end
 
