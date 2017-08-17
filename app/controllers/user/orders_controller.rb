6a0c938105642ddbf27865d7100ff57283741e6e
Joydeep Barman <joydeep.barman@mobiloitte.in>
2017-08-17 19:39:30 +0530

    Another Change

--- app/controllers/user/orders_controller.rb
+++ app/controllers/user/orders_controller.rb
@@ -29,14 +29,14 @@ class User::OrdersController < ApplicationController
   end
 
   def order_payment
-    # @responseSuccessURL = "http://delhi-airport.herokuapp.com/user/orders/success"
-    # @responseFailURL = "http://delhi-airport.herokuapp.com/user/orders/error"
-    # @transactionNotificationURL = "http://delhi-airport.herokuapp.com/user/orders/my_cart"
+    @responseSuccessURL = "http://delhi-airport.herokuapp.com/user/orders/success"
+    @responseFailURL = "http://delhi-airport.herokuapp.com/user/orders/error"
+    @transactionNotificationURL = "http://delhi-airport.herokuapp.com/user/orders/my_cart"
      @cart_orders = current_user.orders.where("is_paid = ?" ,false)
      @sum =  @cart_orders.where("is_paid = ?" ,false).includes(:orderable).map{|o| o.orderable.price}.compact.sum 
-     @responseSuccessURL = "http://localhost:3000/user/orders/success"
-     @responseFailURL = "http://localhost:3000/user/orders/error"
-     @transactionNotificationURL = "http://localhost:3000/user/orders/my_cart"
+     # @responseSuccessURL = "http://localhost:3000/user/orders/success"
+     # @responseFailURL = "http://localhost:3000/user/orders/error"
+     # @transactionNotificationURL = "http://localhost:3000/user/orders/my_cart"
      
      
     # redirect_to success_user_orders_path(order_ids: @cart_orders.pluck(:id) )
